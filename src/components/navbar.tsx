5e7952200a73c1f6b288e9cd9aa5800ce0739588
etrobot@outlook.com <etrobot@outlook.com>
2024-07-01 12:10:38 +0800

    modify home href

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
         <div className="md:sticky md:top-0 md:z-50 md:flex md:items-center md:justify-between md:w-full md:h-12 md:p-2 md:shrink-0 fixed bottom-0 z-50 w-full p-2 flex items-center justify-between bg-background bg-opacity-80">
             <ThemeToggle />
             <nav className='max-w-xs mx-auto flex gap-4'>
-                <Link href='/'>Home</Link>
+                <Link href='/?startCursor=0'>Home</Link>
                 {session && (
                     <>
                         <Link href='/favor'>Favorites</Link>
